a6250be0a9c6b4eb7057da1f616be923b9ca1d9c
paulosmargarites553@gmail.com <you@example.com>
2026-07-07 14:38:48 +0300

    Start of deployment

--- src/main/java/com/autoqa/service/EmailService.java
+++ src/main/java/com/autoqa/service/EmailService.java
@@ -17,16 +17,18 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
+
     public void sendPasswordResetEmail(String to, String token) {
-        String resetUrl = "http://localhost:5173/reset-password?token=" + token;
-        
+        String resetUrl = frontendUrl + "/reset-password?token=" + token;
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject("Auto QA - Password Reset Request");
-        message.setText("To reset your password, please click the link below:\n\n" 
-                + resetUrl + "\n\nIf you did not request this, please ignore this email.");
-        
+        message.setText("To reset your password, please click the link below:\n\n"+ resetUrl + "\n\nIf you did not request this, please ignore this email.");
+
         mailSender.send(message);
     }
 }
